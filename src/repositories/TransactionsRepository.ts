0034c99640cada86cfcc10043afa664f7122517b
Tadeu Tupinamba <tadeu.tupinamba@IBOOK-80075.local>
2020-06-28 21:01:49 -0300

    resolucao do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.transactions.reduce(
+      (previous, current) => {
+        if (current.type === 'income') {
+          const income = previous.income + current.value;
+          return {
+            income,
+            outcome: previous.outcome,
+            total: income - previous.outcome,
+          };
+        }
+
+        const outcome = previous.outcome + current.value;
+        return {
+          income: previous.income,
+          outcome,
+          total: previous.income - outcome,
+        };
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
